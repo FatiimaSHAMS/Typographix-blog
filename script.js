063e3f638a02e9c08c20b6f8c6b4a98722c864fb
FatiimaSHAMS <fatimashams.0@gmail.com>
2024-08-09 12:53:19 -0700

    navbar style

---
+++ script.js
@@ -0,0 +1,25 @@
+// Function to check if page is scrolled and adjust the logo size
+function checkScroll() {
+    const navbar=document.getElementById("navbar");
+    const logo=document.getElementById ("logo");
+    let scrollPosition= window.scrollY;
+
+    //Add/remove 'scrolled' class based on scroll position
+    if(scrollPosition > 20) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+
+    //Calculate new font size based on scroll position
+    let newSize= 3 - (scrollPosition * 0.03);// Decrease by 0.03 rem for every 1px scrolled 
+ 
+    // Clamping the font size between 1.5rem and 3rem
+    newSize = Math.max(1.5, newSize);
+    newSize = Math.min(3,newSize);
+
+    logo.style.fontSize = newSize + "rem";
+}
+
+// Event Listener for scroll event 
+window.addEventListener('scroll', checkScroll);
